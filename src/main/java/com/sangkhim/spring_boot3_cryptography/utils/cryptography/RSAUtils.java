1e1a4558529078e935f0983eaf58a6c2f1c6b01d
sangkhim <sangkhim.allweb@gmail.com>
2023-08-09 17:21:52 +0700

    update RSA

--- src/main/java/com/sangkhim/spring_boot3_cryptography/utils/cryptography/RSAUtils.java
+++ src/main/java/com/sangkhim/spring_boot3_cryptography/utils/cryptography/RSAUtils.java
@@ -2,10 +2,15 @@ package com.sangkhim.spring_boot3_cryptography.utils.cryptography;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -47,15 +52,35 @@ public final class RSAUtils {
     return "";
   }
 
-  public static String decryptMessage(String encryptedMessgae, PrivateKey privateKey) {
+  public static String encryptMessage(String plainText, String publicKeyAsString)
+      throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+    byte[] publicKeyAsByte = Base64.getDecoder().decode(publicKeyAsString);
+    PublicKey publicKey =
+        KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKeyAsByte));
+
+    return encryptMessage(plainText, publicKey);
+  }
+
+  public static String decryptMessage(String encryptedMessage, PrivateKey privateKey) {
     try {
       Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-512ANDMGF1PADDING");
       cipher.init(Cipher.DECRYPT_MODE, privateKey);
-      byte[] decrypt = cipher.doFinal(Base64.getDecoder().decode(encryptedMessgae));
+      byte[] decrypt = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
       return new String(decrypt);
     } catch (Exception e) {
       e.printStackTrace();
     }
     return "";
+  }
+
+  public static String decryptMessage(String encryptedMessage, String privateKeyAsString)
+      throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+    byte[] privateKeyAsByte = Base64.getDecoder().decode(privateKeyAsString);
+    PrivateKey privateKey =
+        KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(privateKeyAsByte));
+
+    return decryptMessage(encryptedMessage, privateKey);
   }
 }
