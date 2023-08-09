aa5f872d3e0e938966fb188dd975f76702a9a82e
sangkhim <sangkhim.allweb@gmail.com>
2023-08-09 09:00:54 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# spring-boot3-cryptography
+# Spring Boot 3 Cryptography
+
+## swagger
+
+    http://localhost:8080/api/
+
+## h2 database console
+
+    http://localhost:8080/h2-console
+
+## other
+
+    Google Java Format
+    SonaLint
+    CheckStyle-IDEA
+    Disable import * (Settings -> Editor -> Code Syle -> Java -> Imports)
---
+++ src/main/java/com/sangkhim/spring_boot3_cryptography/SpringBoot3CryptographyApplication.java
@@ -0,0 +1,12 @@
+package com.sangkhim.spring_boot3_cryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3CryptographyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot3CryptographyApplication.class, args);
+  }
+}
